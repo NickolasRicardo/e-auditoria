a043752f6563786f35e1e1a67ee4c23804875943
NickolasRicardo <nickolas@amexassessoria.com>
2022-06-26 20:54:36 -0300

    Create relatorios page

--- FrontEnd/src/pages/Home/index.tsx
+++ FrontEnd/src/pages/Home/index.tsx
@@ -1,10 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
+import TabelaRelatorio from './../../components/TabelaRelatorio';
+import { Col, Form, Row, Select } from 'antd';
+import Services from './services';
+const { Option } = Select;
 
 const Home: React.FC = () => {
+  const opcoes = [
+    { id: 1, nome: 'Clientes em atraso' },
+    { id: 2, nome: 'Filmes nunca alugados' },
+    { id: 3, nome: 'Filmes mais alugados no ano passado' },
+    { id: 4, nome: 'Filmes menos alugados na ultima semana' },
+    { id: 5, nome: 'Segundo Cliente que mais alugou filmes' },
+  ];
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState(0);
+  const [data, setData] = useState<[]>([]);
+  const [quantidade, setQuantidade] = useState<number | undefined>(undefined);
+
+  const LoadingRelatorio = async (e: any) => {
+    let services = new Services();
+    if (e === 1) {
+      let { error, response } = await services.GetClientesEmAtraso();
+      if (!error && response) {
+        setData(response.data);
+        setQuantidade(response.quantidade);
+      }
+    }
+    if (e === 2) {
+      let { error, response } = await services.GetFilmesNaoAlugados();
+      if (!error && response) {
+        setData(response.data);
+        setQuantidade(response.quantidade);
+      }
+    }
+    if (e === 3) {
+      let { error, response } = await services.GetFilmesMaisAlugados();
+      if (!error && response) {
+        setData(response);
+      }
+    }
+    if (e === 4) {
+      let { error, response } = await services.GetFilmesMenosAlugados();
+      if (!error && response) {
+        setData(response);
+      }
+    }
+    if (e === 5) {
+      let { error, response } = await services.GetClienteQueMaisAlugou();
+      if (!error && response) {
+        setData(response);
+      }
+    }
+  };
+
   return (
     <S.Container>
-      <h1>Home</h1>
+      <Row>
+        <Col span={6}>
+          <h1 style={{ width: '100%' }}>Relatórios</h1>
+        </Col>
+        <Col span={6}>
+          <Select
+            optionFilterProp="children"
+            style={{ minWidth: '100%', padding: 5 }}
+            showSearch
+            placeholder={'Selecione o relatório desejado'}
+            onSelect={(e: any) => LoadingRelatorio(e)}
+          >
+            {opcoes &&
+              opcoes.map((item, index) => {
+                return (
+                  <Option key={index} value={item.id}>
+                    {item.nome}
+                  </Option>
+                );
+              })}
+          </Select>
+        </Col>
+        <Col span={6}></Col>
+        <Col span={12}>
+          <TabelaRelatorio data={data} quantidade={quantidade} />
+        </Col>
+      </Row>
     </S.Container>
   );
 };
--- FrontEnd/src/pages/Home/services.ts
+++ FrontEnd/src/pages/Home/services.ts
@@ -1,13 +1,66 @@
 import api from '../../http/api';
 
-export interface IInterfaceResponse {}
+interface IRelatorio {
+  quantidade: number | undefined;
+  data: [];
+}
+
+export interface IInterfaceResponse {
+  error: boolean;
+  response?: IRelatorio;
+}
+
+export interface IInterfaceResponseWithoutQuantidade {
+  error: boolean;
+  response?: [];
+}
 
 export interface IRequestInterface {}
 
 class Services {
-  public async UpdatePessoaPerfilByPessoaId(data: IRequestInterface) {
+  public async GetClientesEmAtraso(): Promise<IInterfaceResponse> {
     return await api
-      .post<IInterfaceResponse>('', data)
+      .get<IRelatorio>('/api/Relatorio/ClientesEmAtraso')
+      .then(response => {
+        let resposta: IRelatorio = {} as IRelatorio;
+
+        resposta.data = response.data.data;
+        resposta.quantidade = response.data.quantidade;
+
+        return {
+          error: false,
+          response: resposta,
+        };
+      })
+      .catch(error => {
+        return {
+          error: true,
+        };
+      });
+  }
+  public async GetFilmesNaoAlugados(): Promise<IInterfaceResponse> {
+    return await api
+      .get<IRelatorio>('/api/Relatorio/FilmesNaoAlugados')
+      .then(response => {
+        let resposta: IRelatorio = {} as IRelatorio;
+
+        resposta.data = response.data.data;
+        resposta.quantidade = response.data.quantidade;
+
+        return {
+          error: false,
+          response: resposta,
+        };
+      })
+      .catch(error => {
+        return {
+          error: true,
+        };
+      });
+  }
+  public async GetFilmesMaisAlugados(): Promise<IInterfaceResponseWithoutQuantidade> {
+    return await api
+      .get('/api/Relatorio/FilmesMaisAlugados')
       .then(response => {
         return {
           error: false,
@@ -17,7 +70,38 @@ class Services {
       .catch(error => {
         return {
           error: true,
-          response: [],
+        };
+      });
+  }
+  public async GetFilmesMenosAlugados(): Promise<IInterfaceResponseWithoutQuantidade> {
+    return await api
+      .get('/api/Relatorio/FilmesMenosAlugados')
+      .then(response => {
+        return {
+          error: false,
+          response: response.data,
+        };
+      })
+      .catch(error => {
+        return {
+          error: true,
+        };
+      });
+  }
+  public async GetClienteQueMaisAlugou(): Promise<IInterfaceResponseWithoutQuantidade> {
+    return await api
+      .get('/api/Relatorio/ClienteQueMaisAlugou')
+      .then(response => {
+        let array = [] as any;
+        array.push(response.data);
+        return {
+          error: false,
+          response: array,
+        };
+      })
+      .catch(error => {
+        return {
+          error: true,
         };
       });
   }
